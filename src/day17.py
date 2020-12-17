aed15e64939e33a563e88c9687535d8ef7fc2b5a
j-carson <44308120+j-carson@users.noreply.github.com>
2020-12-17 08:47:52 -0800

    refactor cut-n-paste

--- src/day17.py
+++ src/day17.py
@@ -1,3 +1,6 @@
+from itertools import product
+
+
 class Range:
     def __init__(self, low=0, high=0):
         self.low = low
@@ -15,28 +18,49 @@ class Range:
             yield i
 
 
+class DimensionalSpace:
+    def __init__(self, *ranges, ndim):
+        self.ranges = list(ranges)
+        while len(self.ranges) < ndim:
+            self.ranges.append(Range(0, 0))
+
+    def __iter__(self):
+        args = (r for r in self.ranges)
+        for j in product(*args):
+            yield j
+
+    def expand1(self):
+        for r in self.ranges:
+            r.expand1()
+
+
 class Grid:
-    def __init__(self, my_input):
+    def __init__(self, my_input, ndim):
+
         self.grid = set()
+        zero_pad = [0 for _ in range(ndim - 2)]
+
         for row, line in enumerate(my_input.splitlines()):
             for col, ch in enumerate(line):
                 if ch == "#":
                     # active
-                    self.grid.add((row, col, 0))
+                    self.grid.add(tuple([row, col] + zero_pad))
 
-        self.x_range = Range(0, row)
-        self.y_range = Range(0, col)
-        self.z_range = Range(0, 0)
+        x_range = Range(0, row)
+        y_range = Range(0, col)
+        self.space = DimensionalSpace(x_range, y_range, ndim=ndim)
 
-    def count_neighbors(self, x, y, z):
+    def count_neighbors(self, coord):
         count = 0
-        for ix in range(x - 1, x + 2):
-            for iy in range(y - 1, y + 2):
-                for iz in range(z - 1, z + 2):
-                    if all((ix == x, iy == y, iz == z)):
-                        continue
-                    if (ix, iy, iz) in self.grid:
-                        count += 1
+        search_area = (range(ii - 1, ii + 2) for ii in coord)
+        for search_point in product(*search_area):
+            if search_point == coord:
+                continue
+            if search_point in self.grid:
+                count += 1
+                # only care if is two or three, no sense going past here
+                if count > 3:
+                    return count
         return count
 
     def cycle(self):
@@ -48,75 +72,14 @@ class Grid:
         """
         new_grid = set()
 
-        self.x_range.expand1()
-        self.y_range.expand1()
-        self.z_range.expand1()
-
-        for x in self.x_range:
-            for y in self.y_range:
-                for z in self.z_range:
-                    am_active = (x, y, z) in self.grid
-                    neighbors = self.count_neighbors(x, y, z)
-                    if am_active and (neighbors in (2, 3)):
-                        new_grid.add((x, y, z))
-                    elif (not am_active) and (neighbors == 3):
-                        new_grid.add((x, y, z))
-
-        self.grid = new_grid
-
-
-class Grid4D:
-    def __init__(self, my_input):
-        self.grid = set()
-        for row, line in enumerate(my_input.splitlines()):
-            for col, ch in enumerate(line):
-                if ch == "#":
-                    # active
-                    self.grid.add((row, col, 0, 0))
-
-        self.x_range = Range(0, row)
-        self.y_range = Range(0, col)
-        self.z_range = Range(0, 0)
-        self.w_range = Range(0, 0)
-
-    def count_neighbors(self, x, y, z, w):
-        count = 0
-        for ix in range(x - 1, x + 2):
-            for iy in range(y - 1, y + 2):
-                for iz in range(z - 1, z + 2):
-                    for iw in range(w - 1, w + 2):
-                        if all((ix == x, iy == y, iz == z, iw == w)):
-                            continue
-                        if (ix, iy, iz, iw) in self.grid:
-                            count += 1
-                            if count > 3:
-                                return count
-        return count
-
-    def cycle(self):
-        """
-        If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active.
-                Otherwise, the cube becomes inactive.
-        If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active.
-                Otherwise, the cube remains inactive.
-        """
-        new_grid = set()
-
-        self.x_range.expand1()
-        self.y_range.expand1()
-        self.z_range.expand1()
-        self.w_range.expand1()
-
-        for x in self.x_range:
-            for y in self.y_range:
-                for z in self.z_range:
-                    for w in self.w_range:
-                        am_active = (x, y, z, w) in self.grid
-                        neighbors = self.count_neighbors(x, y, z, w)
-                        if am_active and (neighbors in (2, 3)):
-                            new_grid.add((x, y, z, w))
-                        elif (not am_active) and (neighbors == 3):
-                            new_grid.add((x, y, z, w))
+        self.space.expand1()
+        for coord in self.space:
+            am_active = coord in self.grid
+            neighbors = self.count_neighbors(coord)
+            if am_active and (neighbors in (2, 3)):
+                new_grid.add(coord)
+            elif (not am_active) and (neighbors == 3):
+                new_grid.add(coord)
 
         self.grid = new_grid
 
@@ -132,13 +95,13 @@ if __name__ == "__main__":
 #..##.##
 #.#.####"""
 
-    g = Grid(my_input)
+    g = Grid(my_input, ndim=3)
     for _ in range(6):
         g.cycle()
 
     print(len(g.grid))
 
-    g = Grid4D(my_input)
+    g = Grid(my_input, ndim=4)
     for _ in range(6):
         g.cycle()
 
