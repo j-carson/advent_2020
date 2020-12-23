73f9aa8be770aaa51213f22d6059a250d9e5d92e
j-carson <44308120+j-carson@users.noreply.github.com>
2020-12-22 20:31:46 -0800

    narrow search area better

--- src/day17.py
+++ src/day17.py
@@ -1,67 +1,34 @@
 from itertools import product
 
 
-class Range:
-    def __init__(self, low=0, high=0):
-        self.low = low
-        self.high = high
-
-    def __repr__(self):
-        return f"Range(low={self.low},high={self.high})"
-
-    def expand1(self):
-        self.low -= 1
-        self.high += 1
-
-    def __iter__(self):
-        for i in range(self.low, self.high + 1):
-            yield i
-
-
-class DimensionalSpace:
-    def __init__(self, *ranges, ndim):
-        self.ranges = list(ranges)
-        while len(self.ranges) < ndim:
-            self.ranges.append(Range(0, 0))
-
-    def __iter__(self):
-        args = (r for r in self.ranges)
-        for j in product(*args):
-            yield j
-
-    def expand1(self):
-        for r in self.ranges:
-            r.expand1()
-
-
 class Grid:
     def __init__(self, my_input, ndim):
-
+        
         self.grid = set()
         zero_pad = [0 for _ in range(ndim - 2)]
 
         for row, line in enumerate(my_input.splitlines()):
             for col, ch in enumerate(line):
-                if ch == "#":
-                    # active
+                if ch == "#": # active
                     self.grid.add(tuple([row, col] + zero_pad))
+                    
+                    
+    @staticmethod
+    def get_neighbors(coord):
+        neighbors = (range(ii - 1, ii + 2) for ii in coord)
+        for nn in product(*neighbors):
+            if nn != coord: # coord is not it's own neighbor
+                yield nn
 
-        x_range = Range(0, row)
-        y_range = Range(0, col)
-        self.space = DimensionalSpace(x_range, y_range, ndim=ndim)
-
+                
     def count_neighbors(self, coord):
+        "counts active neighbors"
         count = 0
-        search_area = (range(ii - 1, ii + 2) for ii in coord)
-        for search_point in product(*search_area):
-            if search_point == coord:
-                continue
+        for search_point in self.get_neighbors(coord):
             if search_point in self.grid:
                 count += 1
-                # only care if is two or three, no sense going past here
-                if count > 3:
-                    return count
         return count
+    
 
     def cycle(self):
         """
@@ -70,15 +37,25 @@ class Grid:
         If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active.
                 Otherwise, the cube remains inactive.
         """
+        
+        # cubes that are active or that are neighbors of an active
+        # point are the only ones that might possibly change
+        must_consider = self.grid.copy()
+        for coord in self.grid:
+            for nn in self.get_neighbors(coord):
+                must_consider.add(nn)
+        
         new_grid = set()
 
-        self.space.expand1()
-        for coord in self.space:
+        for coord in must_consider:
+            
             am_active = coord in self.grid
-            neighbors = self.count_neighbors(coord)
-            if am_active and (neighbors in (2, 3)):
+            active_neighbors = self.count_neighbors(coord)
+            
+            if am_active and (active_neighbors in (2, 3)):
                 new_grid.add(coord)
-            elif (not am_active) and (neighbors == 3):
+                
+            elif (not am_active) and (active_neighbors == 3):
                 new_grid.add(coord)
 
         self.grid = new_grid
